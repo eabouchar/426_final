abfbbd53e6e0ac5e519598a5a3d9384fcec282bd
Lizzie Abouchar <eaa@live.unc.edu>
2020-10-22 20:38:48 -0400

    registration complete

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "final_project");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect " + mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'hello world')");
+
+
+?>
+
+
+
 <html>
 <head>
     <title></title>
